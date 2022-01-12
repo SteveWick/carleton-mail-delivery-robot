8633c469a27fdf1c02ef88354250818d286f9fbb
Stephen Wicklund <stevewicklund@gmail.com>
2022-01-12 01:11:28 +0000

    receiveRequest.py will now publish the highest priority request. Does not handle multiple high priority requests or consider distance

--- mail_delivery_robot/communication/receiveRequest.py
+++ mail_delivery_robot/communication/receiveRequest.py
@@ -6,23 +6,57 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
 import requests
+
+database = AIOHTTPTransport(
+    url="https://graphql.us.fauna.com/graphql",
+    headers={'authorization': 'Bearer fnAEcsZAjRAAQFZKEY2ld2Lf-oxxylhdza1i8r9j',
+    'Content-Type': 'application/json'})
+
+getPendingRequests = gql(
+"""
+    query Test {
+    requestsByState(state: "pending") {
+        data {
+        id,
+        timestamp,
+        destination,
+        state,
+        priority
+        }
+    }
+    }
+"""
+)
+
 class ReceiveRequest(Node):
   def __init__(self):
         super().__init__('receive_request')
         self.requestPublisher = self.create_publisher(String,'request',2)
-        timer_period = 10  # seconds
+        timer_period = 2  # seconds
         self.timer = self.create_timer(timer_period, self.check_requests)
+        client = Client(transport=database, fetch_schema_from_transport=True)
 
     def check_requests(self):
-        #Check requests
+        if current_request == None:
+            self.requestPublisher.publish(get_highest_priority_pending_requests()[0])
 
-        #TODO:
-        request = requests.get('specific_url')
-        if(request.status_code == 200):
-            requestPublisher.publish(request)
-        else:
-            pass
+    def get_highest_priority_pending_requests(self):
+        requests = client.execute(getOpenRequests)
+        highestPriority = None
+        for request in requests:
+            if request.priority != None and request.priority > highestPriority:
+                highestPriority = request.priority
+        
+        highestPriorityRequests = []
+        for request in requests:
+            if request.priority == highestPriority:
+                highestPriorityRequests.add(request)
+
+        return highestPriorityRequests
+
 
 def main():
     rclpy.init()
