5b14a97164868a76768b43527e384c1e18ebcfcb
chase-scott <91295814+chase-scott@users.noreply.github.com>
2023-01-27 16:05:42 -0500

    Update robotDriver.py
    build for integration tomorrow

--- mail_delivery_robot/control/robotDriver.py
+++ mail_delivery_robot/control/robotDriver.py
@@ -30,110 +30,34 @@ class DriverStateMachine:
     def __init__(self, initialState):
         self.currentState = initialState
 
-    def run(self, distanceFlags):
-        return self.currentState.run(distanceFlags)
-
-    def next(self, distanceFlags, bumperState):
-        self.currentState = self.currentState.next(distanceFlags, bumperState)
-
+    def handleNewDistanceEvent(self, feedback_value, actionPublisher):
+        self.currentState = self.currentState.handleNewDistanceEvent(feedback_value, actionPublisher)
 
 class DriverState:
-    def run(self, distanceFlags):
-        assert 0, "Must be implemented"
-
-    def next(self, distanceFlags, bumperState):
+    def handleNewDistanceEvent(self, feedback_value, actionPublisher):
         assert 0, "Must be implemented"
 
     def toString(self):
         return ""
 
-# Assigned to Jake
-class Docked(DriverState):
-    def run(self, distanceFlags):
-        action = String()
-        return action
-
-    def next(self, distanceFlags, bumperState):
-        pass
-
-    def toString(self):
-        return "Docked"
-
-# Assigned to Jake
 class FindWall(DriverState):
-    def run(self, distanceFlags):
-        action = String()
-        return action
-
-    def next(self, distanceFlags, bumperState):
-        pass
+    def handleNewDistanceEvent(self, feedback_value, actionPublisher):
+        #found wall so change state
+        #TODO this should check that a wall is actually found before switching state.
+        return WallFollowing()
 
     def toString(self):
         return "FindWall"
 
-# Assigned to Chase
 class WallFollowing(DriverState):
-    def run(self, distanceFlags):
+    def handleNewDistanceEvent(self, feedback_value, actionPublisher):
         action = String()
-        # if robot is too far from the wall or is turned away, turn right
-        if ((distanceFlags["tooFar"] or distanceFlags["tightAngle"])):
-            action.data = "sright"
-        # if robot is too close to the wall or is turned towards it, turn left
-        elif ((distanceFlags["tooClose"] or distanceFlags["wideAngle"])):
-            action.data = "sleft"
-        # if all distance flags are good, continue forward
-        else:
-            action.data = "forward"
-        return action
-
-    def next(self, distanceFlags, bumperState):
-        pass
+        action.data = feedback_value
+        actionPublisher.publish(action)
+        return self
 
     def toString(self):
         return "WallFollowing"
-
-# Assigned to Chase
-class IntersectionHandling(DriverState):
-    def run(self, distanceFlags):
-        action = String()
-        return action
-
-    def next(self, distanceFlags, bumperState):
-        pass
-
-    def toString(self):
-        return "IntersectionHandling"
-
-# Assigned to Chase
-class DestinationReached(DriverState):
-    def run(self, distanceFlags):
-        action = String()
-        return action
-
-    def next(self, distanceFlags, bumperState):
-        pass
-
-    def toString(self):
-        return "DestinationReached"
-
-# Assigned to Chase
-class CollisionHandling(DriverState):
-    def run(self, distanceFlags):
-        action = String()
-        return action
-    def next(self, distanceFlags, bumperState):
-        pass
-
-    def toString(self):
-        return "CollisionHandling"
-
-# Initialize states
-DriverStateMachine.Docked = Docked()
-DriverStateMachine.FindWall = FindWall()
-DriverStateMachine.WallFollowing = WallFollowing()
-DriverStateMachine.IntersectionHandling = IntersectionHandling()
-DriverStateMachine.DestinationReached = DestinationReached()
-DriverStateMachine.CollisionHandling = CollisionHandling()
 
 class RobotDriver(Node):
     def __init__(self):
@@ -142,61 +66,41 @@ class RobotDriver(Node):
         self.distance = 0.0
         self.angle = 0.0
 
-        # Events
-        self.distanceFlags = {
-            "tooFar": False,
-            "tooClose": False,
-            "tightAngle": False,
-            "wideAngle": False
-        }
-
         # configure publisher and subscribers
         self.actionPublisher = self.create_publisher(String, 'actions', 2)
-        self.IRSubscriber = self.create_subscription(String, 'preceptions', self.updateDistance, 10)
+        self.IRSubscriber = self.create_subscription(String, 'preceptions', self.updateIRSensor, 10)
         self.bumperEventSubscriber = self.create_subscription(String, 'bumpEvent', self.updateBumperState, 10)
         # TODO These will be implemented in future commits
         # self.mapSubscriber = self.create_subscription(String, 'navigationMap', self.updateMapState, 10)
 
-
-        timer_period = float(magicNumbers['TIMER_PERIOD'])  # Seconds
-        self.timer = self.create_timer(timer_period, self.determineAction)
-
-        # initialize first state TODO
-        self.driverStateMachine = DriverStateMachine(DriverStateMachine.WallFollowing)
-
-    def determineAction(self):
-        # get current action based on current state's run command
-        action = self.driverStateMachine.run(self.distanceFlags)
-        # if action doesn't equa 0, publish it to actions
-        if (action.data != 0):
-            self.get_logger().debug("Publishing: " + action.data)
-            self.actionPublisher.publish(action)
+        # initialize first state
+        self.driverStateMachine = DriverStateMachine(FindWall())
 
     # update the robots distance flags based on data recieved from the IR sensors
-    def updateDistance(self, data):
+    def updateIRSensor(self, data):
+
         if (data.data != "-1"):
-            self.distance = data.data.split(",")[0]
-            self.angle = data.data.split(",")[1]
-            self.distanceFlags["tooFar"] = float(self.distance) > float(magicNumbers['MAX_TARGET_WALL_DISTANCE'])
-            self.distanceFlags["tooClose"] = float(self.distance) < float(magicNumbers['MIN_TARGET_WALL_DISTANCE'])
-            self.distanceFlags["tightAngle"] = float(self.angle) < float(magicNumbers['MIN_TARGET_WALL_ANGLE'])
-            self.distanceFlags["wideAngle"] = float(self.angle) > float(magicNumbers['MAX_TARGET_WALL_ANGLE'])
+            #self.distance = data.data.split(",")[0]
+            #self.angle = data.data.split(",")[1]
+
+            feedback_value = data.data
+            #Make sure to pass it through the decode function first. actionTranslater will have to change in order to be able to handle this new message.
+
+            self.driverStateMachine.handleNewDistanceEvent(feedback_value, self.actionPublisher)
 
         if (DEBUG_MODE):
             self.get_logger().info("Distance: " + str(self.distance) + "Angle: " + str(self.angle))
     
     def updateBumperState(self, data):
         self.bumperState = data.data
-        self.driverStateMachine.next(self.distanceFlags, self.bumperState)
+        #self.driverStateMachine.next(self.distanceFlags, self.bumperState)
         if (DEBUG_MODE):
             self.get_logger().debug("Bumper State: " + self.bumperState)
-
 
 def main():
     rclpy.init()
     robot_driver = RobotDriver()
     rclpy.spin(robot_driver)
-
 
 if __name__ == '__main__':
     main()
